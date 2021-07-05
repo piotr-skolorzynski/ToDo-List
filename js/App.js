0c9741a3cc0391d35198e18c0089ed231ec084bb
Piotr <p.skolorzynski@gmail.com>
2021-07-05 23:06:48 +0200

    create Firebase.js file add firebaseconfig object modify App.js add function to initialize firebase app

--- js/App.js
+++ js/App.js
@@ -1,11 +1,13 @@
 import { prepareDOMElements } from "./DOMElements.js";
 import { prepareDOMEvents } from "./DOMEvents.js";
 import { loadTasksFromLocalStorage } from "./Localstorage.js";
+import { firebaseConfig } from "./Firebase.js";
 
 const main = () => {
     prepareDOMElements();
     prepareDOMEvents();
     loadTasksFromLocalStorage();
+    firebase.initializeApp(firebaseConfig);
 };
 
 document.addEventListener('DOMContentLoaded', main);
